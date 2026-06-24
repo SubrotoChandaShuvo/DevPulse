4d7d591516327e0250aa7901c38d2124d2b223b3
Subroto Chanda Shuvo <subrotochandashuvo@gmail.com>
2026-06-24 19:34:46 +0600

    Update .gitignore to include dist and .env; enhance initDB to create issues table

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,7 +7,7 @@ export const pool = new Pool({
 
 export const initDB = async () => {
   try {
-    await pool.query(`
+        await pool.query(`
             CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
@@ -18,6 +18,19 @@ export const initDB = async () => {
                 updated_at TIMESTAMP DEFAULT NOW()
             );
         `);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS issues (
+            id SERIAL  PRIMARY KEY,
+            title VARCHAR(150) NOT NULL,
+            description TEXT NOT NULL CHECK (LENGTH(description)>=20),
+            type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+            status VARCHAR(20) DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+            reporter_id INTEGER NOT NULL,
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()
+            );
+        `);
+        console.log("Database connected successfully");
   } catch (error) {
     console.error("Error initializing DB:", error);
     throw error;
